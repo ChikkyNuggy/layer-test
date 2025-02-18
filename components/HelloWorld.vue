9496de60221ce58a6738fddbbabb12cb11b60918
ChikkyNuggy <vanpuyvelde.henri@gmail.com>
2025-02-18 16:35:43 +0100

    fetch cat facts using axios in HelloWorld component

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-const { myLayer } = useAppConfig()
+import axios from 'axios';
+
+const response = await axios.get('https://catfact.ninja/fact');
+
 </script>
 
 <template>
   <div>
     <h1>Hi this is my Cool Layer</h1>
-    <pre>{{ myLayer }}</pre>
+    <pre>{{ response.data.fact }}</pre>
   </div>
 </template>
