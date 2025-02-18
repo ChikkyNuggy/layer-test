33f34a41fabb1f598e0ed686e7ba7a509a495121
ChikkyNuggy <vanpuyvelde.henri@gmail.com>
2025-02-18 16:13:51 +0100

    initialize Nuxt layer project with essential configuration and components

---
+++ components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { myLayer } = useAppConfig()
+</script>
+
+<template>
+  <div>
+    <h1>Hi this is my Epic Layer</h1>
+    <pre>{{ myLayer }}</pre>
+  </div>
+</template>
