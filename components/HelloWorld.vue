02d7209829fa4ad6fd8e9db1bb2c9ed0c9b8ecc3
ChikkyNuggy <vanpuyvelde.henri@gmail.com>
2025-02-19 11:53:57 +0100

    comment out axios fetch and response display in HelloWorld component

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import axios from 'axios';
+// import axios from 'axios';
 
-const response = await axios.get('https://catfact.ninja/fact');
+// const response = await axios.get('https://catfact.ninja/fact');
 
 </script>
 
 <template>
   <div>
     <h1>Hi this is my Cool Layer</h1>
-    <pre>{{ response.data.fact }}</pre>
+    <!-- <pre>{{ response.data.fact }}</pre> -->
   </div>
 </template>
